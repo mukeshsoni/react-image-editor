41318e5ecc3b773ed5ee08e7550375cb2f932c68
Mukesh Soni <mukeshsoni@gmail.com>
2025-01-13 19:33:06 +0530

    Constrain the image inside the canvas. Find the scale at which to render the image so that it's inside the canvas.

--- src/ReactImageEditor.tsx
+++ src/ReactImageEditor.tsx
@@ -10,12 +10,33 @@ function renderImageToCanvas(
 
   const ctx = canvasRef.getContext("2d");
   if (ctx) {
-    const canvasWidth = canvasRef.width;
-    const canvasHeight = canvasRef.height;
+    // We restrict the canvas width to the canvas container width
+    const canvasWidth = canvasRef.parentElement?.clientWidth || 800;
+    const canvasHeight = canvasRef.parentElement?.clientHeight || 600;
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    const imageWidth = imageRef.width;
+    const imageHeight = imageRef.height;
+    // We want to fit the image inside the canvas when we first load it
+    // We need to calculate the new image width and height keeping the aspect
+    // ratio intact
+    // If canvas width is more than image width, the scale is > 1. We will restrict the max scale to 1.
+    // If the canvas width is less than the image width, the scale is < 1. Which means we will scale down the
+    // image so that it fits in the canvas.
+    const scaleX = canvasRef.width / imageRef.width;
+    const scaleY = canvasRef.height / imageRef.height;
+    // We pick one of scaleX or scaleY as the final scale value to scale both the width and height
+    // So that the aspect ratio is maintained
+    // We pick the smaller one. If we pick the larger one, the side which needed a smaller scale will
+    // overflow the canvas
+    // The max scale is 1. Otherwise the image will pixelate.
+    const scale = Math.min(Math.min(scaleX, scaleY), 1);
+    const scaledImageWidth = scale * imageWidth;
+    const scaledImageHeight = scale * imageHeight;
+
     // For now we just draw the whole image to the canvas
     // If the canvas size is smaller than the image, a part of the image will be clipped
-    ctx.drawImage(imageRef, 0, 0);
+    ctx.drawImage(imageRef, 0, 0, scaledImageWidth, scaledImageHeight);
   }
 }
 
@@ -56,7 +77,6 @@ export function ReactImageEditor() {
         <input type="file" onChange={handleFileChange} />
       </div>
       <div className="flex-1 border-2">
-        canvas
         <canvas ref={canvasRef} />
       </div>
     </div>
