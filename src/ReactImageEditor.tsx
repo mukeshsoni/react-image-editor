343a4593a2830499731dae5e7e95a1a6e64b9ef0
Mukesh Soni <mukeshsoni@gmail.com>
2025-01-14 17:12:36 +0530

    Able to zoom in and zoom out and reset zoom using buttons

--- src/ReactImageEditor.tsx
+++ src/ReactImageEditor.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useRef, useState } from "react";
 function renderImageToCanvas(
   canvasRef: HTMLCanvasElement | null,
   imageRef: HTMLImageElement,
+  zoomLevel: number,
 ) {
   if (!canvasRef) return;
 
@@ -17,22 +18,8 @@ function renderImageToCanvas(
 
     const imageWidth = imageRef.width;
     const imageHeight = imageRef.height;
-    // We want to fit the image inside the canvas when we first load it
-    // We need to calculate the new image width and height keeping the aspect
-    // ratio intact
-    // If canvas width is more than image width, the scale is > 1. We will restrict the max scale to 1.
-    // If the canvas width is less than the image width, the scale is < 1. Which means we will scale down the
-    // image so that it fits in the canvas.
-    const scaleX = canvasRef.width / imageRef.width;
-    const scaleY = canvasRef.height / imageRef.height;
-    // We pick one of scaleX or scaleY as the final scale value to scale both the width and height
-    // So that the aspect ratio is maintained
-    // We pick the smaller one. If we pick the larger one, the side which needed a smaller scale will
-    // overflow the canvas
-    // The max scale is 1. Otherwise the image will pixelate.
-    const scale = Math.min(Math.min(scaleX, scaleY), 1);
-    const scaledImageWidth = scale * imageWidth;
-    const scaledImageHeight = scale * imageHeight;
+    const scaledImageWidth = zoomLevel * imageWidth;
+    const scaledImageHeight = zoomLevel * imageHeight;
 
     // x coordinate where to start drawing the image
     // if canvas width is larger than the image width, we will try to center the image on the canvas
@@ -56,9 +43,42 @@ function renderImageToCanvas(
   }
 }
 
+// When we load the image for the first time, we want the image to fit inside the canvas
+// If the canvas size is smaller than the image, we want to scale the image down to fit inside the canvas
+function calculateInitialZoomLevel(
+  canvas: HTMLCanvasElement | null,
+  image: HTMLImageElement,
+): number {
+  if (!canvas) {
+    return 1;
+  }
+
+  const canvasWidth = canvas.width;
+  const canvasHeight = canvas.height;
+  const imageWidth = image.width;
+  const imageHeight = image.height;
+
+  // How much do we need to scale the image on the x axis to fit inside the canvas
+  const zoomX = canvasWidth / imageWidth;
+  const zoomY = canvasHeight / imageHeight;
+  // We take the minimum of zoomX and zoomY so that both ends fit in the canvas
+  // If we took the bigger of zoomX and zoomY, the other end would be clipped
+  // And then we restrict the zoom level to 1. So that if zoomX and zoomY are > 1
+  // because the canvas is bigger than the image, we don't enlarge the image and
+  // in the process pixelate it
+  const zoomLelvel = Math.min(Math.min(zoomX, zoomY), 1);
+
+  return zoomLelvel;
+}
+
 export function ReactImageEditor() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [imageFile, setImageFile] = useState<File | null>(null);
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  // The amount to which the image is scaled/zoomed
+  // When the user loads an image, we calculate the zoomLevel so that the image fits in the canvas
+  // Users can then change the zoom level themselves
+  const [zoomLevel, setZoomLevel] = useState(1);
 
   // Set canvas width and height
   useEffect(() => {
@@ -82,15 +102,75 @@ export function ReactImageEditor() {
       // when the browser is done loading the image to our Image instance
       // we try to render it to the canvas
       img.onload = () => {
-        renderImageToCanvas(canvasRef.current, img);
+        const initialZoomLevel = calculateInitialZoomLevel(
+          canvasRef.current,
+          img,
+        );
+        imageRef.current = img;
+        setZoomLevel(initialZoomLevel);
+        renderImageToCanvas(
+          canvasRef.current,
+          imageRef.current,
+          initialZoomLevel,
+        );
       };
     }
   }, [imageFile]);
+  // rerender the image when the zoomLevel has changed
+  useEffect(() => {
+    if (imageRef.current) {
+      renderImageToCanvas(canvasRef.current, imageRef.current, zoomLevel);
+    }
+  }, [zoomLevel]);
+  // When user clicks on the minus (-) button, we zoom out the image a bit
+  // The zoom out is not absolute amount. It will depend on the existing zoomLevel
+  // Otherwise, at lower zoom levels, it will feel like it's zooming out too fast
+  // TODO: We are zooming from the center of the image always. We will fix it in the next iteration
+  function handleZoomOutClick() {
+    // Decrease zoom by 5%
+    const newZoomLevel = zoomLevel - zoomLevel * 0.05;
+    setZoomLevel(newZoomLevel);
+  }
+  function handleZoomInClick() {
+    // Increase zoom by 5%
+    const newZoomLevel = zoomLevel + zoomLevel * 0.05;
+    setZoomLevel(newZoomLevel);
+  }
+  function handleResetZoomClick() {
+    if (imageRef.current) {
+      setZoomLevel(
+        calculateInitialZoomLevel(canvasRef.current, imageRef.current),
+      );
+    }
+  }
 
   return (
     <div className="w-full h-full flex flex-col">
       <div className="p-2">
-        <input type="file" onChange={handleFileChange} />
+        <div className="flex gap-4">
+          <input type="file" onChange={handleFileChange} />
+          <div className="flex gap-4">
+            <button
+              className="px-2 rounded border border-indigo-600"
+              onClick={handleZoomOutClick}
+            >
+              -
+            </button>
+            <button
+              className="px-2 rounded border border-indigo-600"
+              onClick={handleResetZoomClick}
+              title="Reset"
+            >
+              {Math.round(zoomLevel * 100)}%
+            </button>
+            <button
+              className="px-2 rounded border border-indigo-600"
+              onClick={handleZoomInClick}
+            >
+              +
+            </button>
+          </div>
+        </div>
       </div>
       <div className="flex-1 border-2">
         <canvas ref={canvasRef} />
