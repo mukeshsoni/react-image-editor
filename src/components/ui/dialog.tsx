5789e9ba63995c02d109461ab0433a3610b1d059
Mukesh Soni <mukeshsoni@gmail.com>
2026-01-22 19:18:50 +0530

    support for dark theme

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -3,6 +3,7 @@ import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { XIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
+import { useEditorTheme } from "@/components/ui/editor-theme"
 
 function Dialog({
   ...props
@@ -52,12 +53,16 @@ function DialogContent({
 }: React.ComponentProps<typeof DialogPrimitive.Content> & {
   showCloseButton?: boolean
 }) {
+  const { resolvedTheme } = useEditorTheme()
+
   return (
     <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
+          "react-image-editor-portal",
+          resolvedTheme === "dark" && "dark",
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
           className
         )}
